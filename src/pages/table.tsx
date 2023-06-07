1b378f16321864ba47f2a3510dc5e2873150b1b4
serhii-sultanov <sshsultanov@gmail.com>
2023-06-07 19:00:40 +0300

    removed UsersTable component, implemented sorting in SSR, added array of column headers, removed type casting to number, deleted toUsersPage button

--- src/pages/table.tsx
+++ src/pages/table.tsx
@@ -1,40 +1,100 @@
-import { UsersTable } from '@/components/UsersTable';
 import { getAllUsers } from '@/utils/userUtils';
 import { GetServerSideProps } from 'next';
-import Image from 'next/image';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { FC } from 'react';
 import { UserData } from '../../types';
-import close from '../assets/icons/close.svg';
+
+type UserDataKey = keyof UserData | 'name';
+type SortOrder = 'asc' | 'desc' | '';
 
 type TableProps = {
   users: UserData[];
+  sorting: {
+    column: UserDataKey | '';
+    order: SortOrder;
+  };
 };
 
-export const getServerSideProps: GetServerSideProps<TableProps> = async () => {
-  const users = await getAllUsers();
+export const getServerSideProps: GetServerSideProps<TableProps> = async (
+  context,
+) => {
+  const sortBy = context.query.sortBy as UserDataKey | undefined;
+  const sortOrder = context.query.sortOrder as SortOrder | undefined;
+
+  let users = await getAllUsers();
+
+  if (sortBy && sortOrder) {
+    users = users.sort((a, b) => {
+      if (sortBy === 'name') {
+        if (sortOrder === 'asc') {
+          return a.firstName.localeCompare(b.firstName);
+        } else {
+          return b.firstName.localeCompare(a.firstName);
+        }
+      } else {
+        const column = sortBy as keyof UserData;
+        if (sortOrder === 'asc') {
+          return (a[column] as number) - (b[column] as number);
+        } else {
+          return (b[column] as number) - (a[column] as number);
+        }
+      }
+    });
+  }
+
   return {
     props: {
       users,
+      sorting: {
+        column: sortBy || '',
+        order: sortOrder || '',
+      },
     },
   };
 };
 
-const Table: FC<TableProps> = ({ users }) => {
+const Table: FC<TableProps> = ({ users, sorting }) => {
+  const columns: UserDataKey[] = ['name', 'age', 'weight', 'height'];
+
+  const router = useRouter();
+
+  const handleColumnClick = (column: keyof UserData | 'name') => () => {
+    const order = sorting.order === 'asc' ? 'desc' : 'asc';
+    router.push({
+      pathname: '/table',
+      query: { sortBy: column, sortOrder: order },
+    });
+  };
+
   return (
-    <>
-      <div className="w-12 fixed top-12 right-12">
-        <Link href="/users?page=1">
-          <Image
-            className="w-full hover:transform hover:scale-110 transition-all duration-300"
-            priority
-            src={close}
-            alt="Back to Users List"
-          />
-        </Link>
-      </div>
-      <UsersTable users={users} />
-    </>
+    <table className="table-fixed w-full md:w-3/4 m-auto">
+      <thead>
+        <tr className="border-b border-b-slate-300 bg-slate-200 text-slate-900 uppercase text-lg">
+          {columns.map((column) => (
+            <th
+              key={column}
+              className="py-3 hover:cursor-pointer"
+              onClick={handleColumnClick(column)}
+            >
+              {column} <span>&#10607;</span>
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr
+            key={user.id}
+            className="border-b border-b-slate-300 bg-slate-50 text-center"
+          >
+            <td className="text-base text-slate-900 font-semibold py-3">{`${user.firstName} ${user.lastName}`}</td>
+            <td className="text-base text-slate-900 py-3">{user.age}</td>
+            <td className="text-base text-slate-900 py-3">{user.weight}</td>
+            <td className="text-base text-slate-900 py-3">{user.height}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
